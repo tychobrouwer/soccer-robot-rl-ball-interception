c01bc8c5ac82cdcb9aa5c16339dc0b079b0b9353
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-09-13 15:33:13 +0200

    initial ue project

---
+++ OmniWheelRobot/Source/OmniWheelRobot/OmniWheelRobot.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OmniWheelRobot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OmniWheelRobot, "OmniWheelRobot" );
---
+++ OmniWheelRobot/Source/OmniWheelRobot/OmniWheelRobotGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OmniWheelRobotGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OMNIWHEELROBOT_API AOmniWheelRobotGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
