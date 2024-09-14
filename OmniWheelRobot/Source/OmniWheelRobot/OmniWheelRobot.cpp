f648b79a941584803f74a7afd5d2085aec3203a7
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-09-14 20:07:10 +0200

    added omni robot + no friction

--- OmniWheelRobot/Source/OmniWheelRobot/OmniWheelRobot.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "OmniWheelRobot.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OmniWheelRobot, "OmniWheelRobot" );
