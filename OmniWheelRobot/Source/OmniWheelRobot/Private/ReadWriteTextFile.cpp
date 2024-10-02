5a9c8840425c7ab7fd0d345be8b80c82ffb33c7b
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2024-10-02 15:18:25 +0200

    fix readling line from file + making functions to clean up main event loop + more test results

--- OmniWheelRobot/Source/OmniWheelRobot/Private/ReadWriteTextFile.cpp
+++ OmniWheelRobot/Source/OmniWheelRobot/Private/ReadWriteTextFile.cpp
@@ -6,24 +6,31 @@
 
 FString UReadWriteTextFile::ReadStringFromFile(const FString &FilePath,
                                                bool &bOutSuccess,
-                                               FString &OutInfoMessage) {
-  if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*FilePath)) {
+                                               FString &OutInfoMessage)
+{
+  if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*FilePath))
+  {
     bOutSuccess = false;
     OutInfoMessage = FString::Printf(
         TEXT("Read String From File Failed - File doesn't exist - '%s'"),
         *FilePath);
+
+    UE_LOG(LogTemp, Error, TEXT("File doesn't exist"));
 
     return "";
   };
 
   FString RetString = "";
 
-  if (!FFileHelper::LoadFileToString(RetString, *FilePath)) {
+  if (!FFileHelper::LoadFileToString(RetString, *FilePath))
+  {
     bOutSuccess = false;
     OutInfoMessage =
         FString::Printf(TEXT("Read String From File Failed - Was not able to "
                              "read file. Is this a text file? - '%s'"),
                         *FilePath);
+
+    UE_LOG(LogTemp, Error, TEXT("Was not able to read file. Is this a text file?"));
 
     return "";
   }
@@ -38,18 +45,27 @@ FString UReadWriteTextFile::ReadStringFromFile(const FString &FilePath,
 TArray<float> UReadWriteTextFile::ReadLineFromInput(const FString &FilePath,
                                                     const int32 LineIndex,
                                                     bool &bOutSuccess,
-                                                    FString &OutInfoMessage) {
+                                                    FString &OutInfoMessage)
+{
   const FString FileContent =
       ReadStringFromFile(FilePath, bOutSuccess, OutInfoMessage);
+
+  if (!bOutSuccess)
+  {
+    return TArray<float>();
+  }
 
   TArray<FString> Lines;
   const int32 LineCount = FileContent.ParseIntoArrayLines(Lines, true);
 
-  if (LineIndex >= LineCount) {
+  if (LineIndex >= LineCount)
+  {
     bOutSuccess = false;
     OutInfoMessage = FString::Printf(
         TEXT("Read Line From Input Failed - Line index out of bounds - '%s'"),
         *FilePath);
+
+    UE_LOG(LogTemp, Error, TEXT("Line index out of bounds"));
 
     return TArray<float>();
   }
@@ -62,14 +78,26 @@ TArray<float> UReadWriteTextFile::ReadLineFromInput(const FString &FilePath,
       InputLineString.ParseIntoArray(InputStringValues, TEXT(","), true);
 
   TArray<float> InputValues;
-  for (int32 i = 0; i < InputCount; i++) {
-    InputValues.Insert(i, FCString::Atof(*InputStringValues[i]));
+  for (int32 i = 0; i < InputCount; i++)
+  {
+    if (InputStringValues.IsValidIndex(i))
+    {
+      float Value = FCString::Atof(*InputStringValues[i]);
+      InputValues.Insert(Value, i);
+    }
+    else
+    {
+      UE_LOG(LogTemp, Error, TEXT("Index %d out of bounds for InputStringValues"), i);
+    }
   }
 
-  if (InputCount < 10) {
+  if (InputCount < 10)
+  {
     bOutSuccess = true;
     OutInfoMessage = FString::Printf(
         TEXT("Input file did not contain all setpoint variables"));
+
+    UE_LOG(LogTemp, Error, TEXT("Input file did not contain all setpoint variables"));
 
     return TArray<float>();
   }
@@ -84,15 +112,19 @@ TArray<float> UReadWriteTextFile::ReadLineFromInput(const FString &FilePath,
 void UReadWriteTextFile::WriteStringToFile(const FString &FilePath,
                                            const FString &String,
                                            bool &bOutSuccess,
-                                           FString &OutInfoMessage) {
+                                           FString &OutInfoMessage)
+{
   if (!FFileHelper::SaveStringToFile(String, *FilePath,
                                      FFileHelper::EEncodingOptions::AutoDetect,
-                                     &IFileManager::Get(), FILEWRITE_Append)) {
+                                     &IFileManager::Get(), FILEWRITE_Append))
+  {
     bOutSuccess = false;
     OutInfoMessage = FString::Printf(
         TEXT("Write String To File Failed - Was not able to write to file. Is "
              "your file read-only? Is the path valid? - '%s'"),
         *FilePath);
+
+    UE_LOG(LogTemp, Error, TEXT("Was not able to write to file. Is your file read-only? Is the path valid?"));
 
     return;
   }
